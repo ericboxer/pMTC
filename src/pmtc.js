dc3b4963ab7f8bdf970db2b7f65650a630ecafeb
Eric Boxer <eboxhead@gmail.com>
2019-06-08 11:29:25 -0400

    Added support for sending just legacy data

--- src/pmtc.js
+++ src/pmtc.js
@@ -1,3 +1,13 @@
+/**
+ * @file pmtc.js
+ *
+ * @summary A reader for full frame only MTC
+ * @author Eric Boxer <eric@ericboxer.net>
+ *
+ * Created at     : 2019-05-22 07:43:32
+ * Last modified  : 2019-06-08 11:25:12
+ */
+
 'use strict'
 
 const dgram = require('dgram')
@@ -32,11 +42,12 @@ const frameratesEnum = {
 }
 
 class PMTC extends EventEmitter {
-  constructor(interfaceAddress, port) {
+  constructor(interfaceAddress, port, legacyOnly = false) {
     super()
     this.address = interfaceAddress || ''
     this.port = port || 5555
     this.conn = dgram.createSocket('udp4')
+    this.legacyOnly = legacyOnly
   }
 
   run() {
@@ -80,39 +91,44 @@ class PMTC extends EventEmitter {
     // Chances are good that this is pMTC message. Like seriously. It's pretty darn accurate.
     if (msg.length == 10 && msg.slice[(0, 3)] == mtcPacket.slice[(0, 3)]) {
       try {
-        // lets grab the frame rate, hour, minute, seconds, and frames from the packet
-        const hours = this._pmtcHourFromHours(msg[5])
-        const fr = this._pmtcFrameRateFromHours(msg[5])
-        const minutes = msg[6]
-        const seconds = msg[7]
-        const frames = msg[8]
+        // If all we want to do is send out the information on a multicast or boradcast address... this is where we do it.
+        if (this.legacyOnly) {
+          this.emit('timecode', msg)
+        } else {
+          // lets grab the frame rate, hour, minute, seconds, and frames from the packet
+          const hours = this._pmtcHourFromHours(msg[5])
+          const fr = this._pmtcFrameRateFromHours(msg[5])
+          const minutes = msg[6]
+          const seconds = msg[7]
+          const frames = msg[8]
 
-        // Now we have all the parts we need. Lets create a few different ways of transmitting the code.
+          // Now we have all the parts we need. Lets create a few different ways of transmitting the code.
 
-        const framerateTC = boxtools.nameFromEnumValue(frameratesEnum, fr)
-        const frDivider = this._pmtcDetermineFrameDivider(framerateTC) // When calculating timecode, what framerate do we need to divide by?
+          const framerateTC = boxtools.nameFromEnumValue(frameratesEnum, fr)
+          const frDivider = this._pmtcDetermineFrameDivider(framerateTC) // When calculating timecode, what framerate do we need to divide by?
 
-        // In JSON
-        const jsonTC = JSON.stringify({
-          hours: hours,
-          minutes: minutes,
-          seconds: seconds,
-          frames: frames,
-        })
+          // In JSON
+          const jsonTC = JSON.stringify({
+            hours: hours,
+            minutes: minutes,
+            seconds: seconds,
+            frames: frames,
+          })
 
-        // In total Frames
-        const totalFrames = this._pmtcCalculateFrames(hours, minutes, seconds, frames, frDivider)
+          // In total Frames
+          const totalFrames = this._pmtcCalculateFrames(hours, minutes, seconds, frames, frDivider)
 
-        // Bult the return object
-        const tcObject = {
-          framerate: framerateTC,
-          json: jsonTC,
-          frame: totalFrames,
-          legacy: [...msg],
+          // Build the return object
+          const tcObject = {
+            framerate: framerateTC,
+            json: jsonTC,
+            frame: totalFrames,
+            legacy: [...msg],
+          }
+
+          // Send it off to the masses!
+          this.emit('timecode', JSON.stringify(tcObject))
         }
-
-        // Send it off to the masses!
-        this.emit('timecode', JSON.stringify(tcObject))
       } catch (e) {
         console.log(e)
       }
@@ -190,7 +206,7 @@ module.exports = {
 // Simple local testing
 
 // if (typeof require != 'undefined' && require.main == module) {
-//   const a = new PMTC('', 5005)
+//   const a = new PMTC('', 5005, true)
 //   a.run()
 
 //   a.on('timecode', (data) => {
